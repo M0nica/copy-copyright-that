68721cc3311fdb7d41c169b12449612d15b16579
Monica Powell <monica@jopwell.com>
2017-10-07 18:22:03 -0400

    returns weather or not a domain is available for the particular word, currently is very binary

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 # views.py
 
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
+import requests
 
 from app import app
 
@@ -11,3 +12,31 @@ def index():
 @app.route('/contribute')
 def about():
     return render_template("contribute.html")
+
+@app.route("/", methods=['POST'])
+def calculate():
+    business_name = str(request.form['business_name'])
+
+
+    return redirect(url_for('results', business_name=business_name))
+
+
+@app.route('/results/<business_name>')
+
+
+def results(business_name):
+    # searches https://domainsdb.info/apidomainsdb/ to determine if domain is available
+    response = requests.get('https://api.domainsdb.info/search?query=' + business_name + '&tld=all')
+    data = response.json()
+
+    if data['total'] == 0:
+        domain_availability = "available"
+    else:
+        domain_availability = "not available"
+
+    # print(data)
+    business_name = business_name
+
+
+
+    return render_template('results.html', business_name=business_name, domain_availability=domain_availability)
